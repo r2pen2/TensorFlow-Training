361e0eed506e782da584f1f759ad428e5245733e
r2pen2 <joedobbelaar@gmail.com>
2023-10-05 19:40:43 -0400

    anal(syis)

--- joe-analysis.py
+++ joe-analysis.py
@@ -3,13 +3,15 @@ from tensorflow import keras
 from tensorflow.keras import layers
 import matplotlib.pyplot as plt
 import numpy as np
+from sklearn.metrics import confusion_matrix
+import itertools
 
 ###########################MAGIC HAPPENS HERE##########################
 # Change the hyper-parameters to get the model performs well
 config = {
-    'batch_size': 64,
+    'batch_size': 128,
     'image_size': (100,100),
-    'epochs': 100,
+    'epochs': 50,
     'optimizer': keras.optimizers.experimental.Adam(5e-4)
 }
 ###########################MAGIC ENDS  HERE##########################
@@ -36,10 +38,10 @@ def data_processing(ds):
             ###########################MAGIC HAPPENS HERE##########################
             # Use dataset augmentation methods to prevent overfitting, 
             layers.RandomFlip("horizontal"),
-            layers.RandomRotation(0.1),
-            layers.RandomZoom(0.1),
-            layers.RandomTranslation(height_factor=0.1, width_factor=0.1),
-            layers.RandomContrast(0.1)
+            layers.RandomRotation(0.3),
+            layers.RandomZoom(0.3),
+            layers.RandomTranslation(height_factor=0.3, width_factor=0.3),
+            layers.RandomContrast(0.3)
             ###########################MAGIC ENDS HERE##########################
         ]
     )
@@ -58,11 +60,11 @@ def build_model(input_shape, num_classes):
     # Use Keras API like `x = layers.XXX()(x)`
     # Hint: Use a Deeper network (i.e., more hidden layers, different type of layers)
     # and different combination of activation function to achieve better result.
-    hidden_units = 64
-    x = layers.Conv2D(64, activation='relu', kernel_size=(3, 3), input_shape=input_shape)(x)
-    x = layers.MaxPooling2D(pool_size=2, strides=1, padding='same')(x)
-    x = layers.Conv2D(64, activation='relu', kernel_size=(3, 3), input_shape=input_shape)(x)
-    x = layers.MaxPooling2D(pool_size=2, strides=1, padding='same')(x)
+    hidden_units = 128
+    x = layers.Conv2D(128, activation='relu', kernel_size=(3, 3), input_shape=input_shape)(x)
+    x = layers.MaxPooling2D(pool_size=2, strides=2, padding='same')(x)
+    x = layers.Conv2D(128, activation='relu', kernel_size=(3, 3), input_shape=input_shape)(x)
+    x = layers.MaxPooling2D(pool_size=2, strides=2, padding='same')(x)
     x = layers.Flatten()(x)
     x = layers.Dense(hidden_units, activation='relu')(x)
     # x = layers.Dense(hidden_units, activation='relu')(x)
@@ -83,7 +85,7 @@ if __name__ == '__main__':
     train_ds, val_ds, test_ds = read_data()
     train_ds = data_processing(train_ds)
     # Build up the ANN model
-    model = build_model(config['image_size']+(3,), 5)
+    model = keras.models.load_model("best_trained_model.keras")
     # Compile the model with optimizer and loss function
     model.compile(
         optimizer=config['optimizer'],
@@ -91,25 +93,66 @@ if __name__ == '__main__':
         metrics=["accuracy"],
     )
     # Fit the model with training dataset
-    history = model.fit(
-        train_ds,
-        epochs=config['epochs'],
-        validation_data=val_ds
-    )
+    # history = model.fit(
+    #     train_ds,
+    #     epochs=config['epochs'],
+    #     validation_data=val_ds
+    # )
     ###########################MAGIC HAPPENS HERE##########################
-    print(history.history)
+    # print(history.history)
     test_loss, test_acc = model.evaluate(test_ds, verbose=2)
     print("\nTest Accuracy: ", test_acc)
     test_images = np.concatenate([x for x, y in test_ds], axis=0)
     test_labels = np.concatenate([y for x, y in test_ds], axis=0)
     test_prediction = np.argmax(model.predict(test_images),1)
     # 1. Visualize the confusion matrix by matplotlib and sklearn based on test_prediction and test_labels
+    cm = confusion_matrix(test_labels, test_prediction)
+
+    # Plot the confusion matrix
+    plt.figure(figsize=(10, 7))
+    plt.imshow(cm, interpolation='nearest', cmap=plt.cm.Blues)
+    plt.title('Confusion Matrix')
+    plt.colorbar()
+
+    # Add labels to the plot
+    classes = ["sunflowers", "dandelion", "daisy", "tulips", "roses"]
+    tick_marks = np.arange(len(classes))
+    plt.xticks(tick_marks, classes, rotation=45)
+    plt.yticks(tick_marks, classes)
+
+    # Display the values in the matrix
+    thresh = cm.max() / 2.
+    for i, j in itertools.product(range(cm.shape[0]), range(cm.shape[1])):
+        plt.text(j, i, cm[i, j],
+                 horizontalalignment="center",
+                 color="white" if cm[i, j] > thresh else "black")
+
+    plt.tight_layout()
+    plt.ylabel('True label')
+    plt.xlabel('Predicted label')
+    plt.show()
+
     # 2. Report the precision and recall for 10 different classes
     # Hint: check the precision and recall functions from sklearn package or you can implement these function by yourselves.
     # 3. Visualize three misclassified images
     # Hint: Use the test_images array to generate the misclassified images using matplotlib
 
+    # Identify Misclassified Images
+    misclassified_indices = np.where(test_prediction != test_labels)[0]
+    # Choose first 3 misclassified images
+    selected_indices = misclassified_indices[:25]
+    # Visualize Misclassified Images
+    fig, axes = plt.subplots(5, 5, figsize=(5, 5))
 
-
+    for i, index in enumerate(selected_indices):
+        # Convert the image to grayscale
+        gray_image = np.mean(test_images[index], axis=-1)
+        row = i // 5
+        col = i % 5
+        axes[row, col].imshow(gray_image.astype(np.uint8), cmap='gray')
+        axes[row, col].set_title(f"True: {test_labels[index]}, Pred: {test_prediction[index]}")
+        axes[row, col].axis('off')
+    plt.tight_layout()
+    plt.show()
 
     ###########################MAGIC HAPPENS HERE##########################
